184f7ffeff6efbfef4a40405cdace2862ef862f9
TrungHieu <nguyentrunghieu250303@gmail.com>
2024-12-18 14:46:29 +0700

    update view

--- ViewModels/CourseViewModel.cs
+++ ViewModels/CourseViewModel.cs
@@ -3,17 +3,23 @@ namespace StudentManagement.ViewModels;
 public class CourseRequest
 {
     [Required]
+    [Display(Name ="Mã lớp")]
     public string? CourseID { get; set; }
     [Required]
+    [Display(Name = "Tên lớp")]
     public string? CourseName { get; set; }
     [Required]
+    [Display(Name = "Giáo viên")]
     public string? Teacher { get; set; }
 }
 
 public class CourseViewModel
 {
     public int Id { get; set; }
+    [Display(Name = "Mã lớp")]
     public string? CourseID { get; set; }
+    [Display(Name = "Tên lớp")]
     public string? CourseName { get; set; }
+    [Display(Name = "Giáo viên")]
     public string? Teacher { get; set; }
 }
--- ViewModels/StudentViewModel.cs
+++ ViewModels/StudentViewModel.cs
@@ -3,20 +3,32 @@ using System.ComponentModel.DataAnnotations;
 namespace StudentManagement.ViewModels;
 public class StudentRequest
 {
+    [Required]
     public IFormFile? Image { get; set; }
+    [Required]
     public string? StudentId { get; set; }
+    [Required]
     public string? StudentName { get; set; }
+    [Required]
     public DateTime DateOfBirth { get; set; }
+    [Display(Name ="Giới tính")]
+    [Required]
     public string? Gender { get; set; }
+    [Required]
     public int CourseId { get; set; }
 }
 public class StudentViewModel{
     public int Id { get; set; }
     public string? ImagePath { get; set; }
     public IFormFile? Image { get; set; }
+    [Display(Name = "Mã sinh viên")]
     public string? StudentId { get; set; }
+    [Display(Name = "Họ tên")]
     public string? StudentName { get; set; }
+    [DataType(DataType.Date)]
+    [Display(Name = "Ngày sinh")]
     public DateTime DateOfBirth { get; set; }
+    [Display(Name = "Giới tính")]
     public string? Gender { get; set; }
     public int CourseId { get; set; }
     public int CourseNAME { get; set; }
